69efcd3882a12acc600c2a95e74f31d3e76034e6
Jordan Holliday <jordan.holliday@tapad.com>
2017-10-19 09:46:20 -0400

    prettify eventapi return

--- src/api/events-api.ts
+++ src/api/events-api.ts
@@ -4,6 +4,22 @@ import { Event } from "../models/event";
 
 export class EventsApi {
     async get(query: string[] = []): Promise<Event[]> {
-        return get("events", query, (raw: any) => raw.events as Event[]);
+		return get("events", query, (raw: any) => prettifyEvents(raw.events as Event[]))
     }
+}
+
+function prettifyEvents(events: Event[]): any[] {
+	return events
+		.map(e => {
+			return {
+				title: e.title,
+				url: e.url,
+				datetime: e.datetime_utc,
+				venue: e.venue.name,
+				city: e.venue.city,
+				minPrice: e.stats.lowest_price,
+				listingCount: e.stats.listing_count
+			}
+		})
+		.sort((event1, event2) => +event1.minPrice - +event2.minPrice);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import * as express from "express";
 import { IncomingMessage, ServerResponse } from "http";
 import * as url from "url";
-import { getClientIp } from "request-ip";
 
 import { EventsApi } from "./api";
 import { Event } from "./models";
 
-const api = new EventsApi();
+const eventsApi = new EventsApi();
 const server = express();
 server.get("", requestHandler).listen(3000);
 
 async function requestHandler(req: IncomingMessage, res: ServerResponse): Promise<void> {
     let events;
     try {
-        events = await api.get(getParamsFromRequest(req));
+        events = await eventsApi.get(getParamsFromRequest(req));
     } catch (e) {
         res.writeHead(500);
         return res.end();
@@ -22,12 +21,20 @@ async function requestHandler(req: IncomingMessage, res: ServerResponse): Promis
 }
 
 function getParamsFromRequest(req: IncomingMessage): string[] {
-    const queryParams = url.parse(req.url!, true).query;
-    const ip = getClientIp(req);
-    return [
-        "per_page=100", 
-        `geoip=${ queryParams.geoip || ip || 10000 }`, 
-        `lowest_price.lte=${ queryParams.price || 35 }`, 
-        "datetime_utc.lte=2017-10-31"
-    ];
+	const queryParams = url.parse(req.url!, true).query;
+	return [
+		"per_page=100",
+		...Object.keys(queryParams).map((key) => `${ getQueryKey(key) }=${ queryParams[key] }`)
+	];
+}
+
+function getQueryKey(key: string): string {
+	switch (key) {
+		case "price":
+			return "lowest_price.lte";
+		case "date":
+			return "datetime_utc.lte";
+		default:
+			return key;
+	}
 }
